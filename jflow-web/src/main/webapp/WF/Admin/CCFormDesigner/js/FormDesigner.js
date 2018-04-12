e0345909fd3f92fb191b196731740e34d9ac889c
ccflow..liyan <3301790953@qq.com>
2018-04-13 06:45:33 +0800

    no commit message

--- jflow-web/src/main/webapp/WF/Admin/CCFormDesigner/js/FormDesigner.js
+++ jflow-web/src/main/webapp/WF/Admin/CCFormDesigner/js/FormDesigner.js
@@ -307,6 +307,8 @@ function showFigurePropertyWin(figure) {
     }
 
     if (shap == 'HyperLink') {
+
+        var en=  new Entity("")
         var url = '../../Comm/EnOnly.htm?EnName=BP.Sys.FrmUI.FrmLink&PKVal=' + figure.CCForm_MyPK;
         CCForm_ShowDialog(url, '超链接属性');
         return;

760016ac236108ff694858662c1c31d12e1217a8
ccflow..liyan <3301790953@qq.com>
2018-04-28 20:21:40 +0800

    no commit message

--- jflow-web/src/main/webapp/WF/MyFlowGener.js
+++ jflow-web/src/main/webapp/WF/MyFlowGener.js
@@ -1224,8 +1224,6 @@ function GenerWorkNode() {
         dataType: 'html',
         success: function (data) {
 
-        	
-        	 
             if (data.indexOf('err@') == 0) {
                 alert(data);
                 console.log(data);
@@ -1241,10 +1239,6 @@ function GenerWorkNode() {
                 alert(" GenerWorkNode转换JSON失败,请查看控制台日志,或者联系管理员.");
                 return;
             }
-            
-            var mainTable=flowData["MainTable"];          
-            
-            console.log(mainTable);
 
             var node = flowData.WF_Node[0];
 

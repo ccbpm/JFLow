c385f155cba34d1de9e603f97d8ecd1a6f2386da
袁丽娜 <nayuanli@126.com>
2018-05-10 11:59:00 +0800

    no commit message

---
+++ jflow-core/src/main/java/BP/Tools/Constant4Comm.java
@@ -0,0 +1,271 @@
+/**
+ * Project Name:app-engine
+ * File Name:Constant4Comm.java
+ * Package Name:com.bwda.engine.common
+ * Date:2017年10月19日
+ * Copyright (c) 2017, 江苏保旺达软件有限公司 All Rights Reserved.
+ *
+ */
+package BP.Tools;
+
+/**
+ * 共通常量定义
+ *
+ * @author fanchengliang
+ * @date 2017年10月19日 
+ */
+public class Constant4Comm {
+    
+    
+    /**
+     * 符号.
+     */
+    public static final String STR_DOT = ".";
+    
+    /**
+     * 符号,
+     */
+    public static final String STR_COMMA = ",";
+    
+    /**
+     * 符号!
+     */
+    public static final String STR_IMPORTAMT = "!";
+    
+    /**
+     * 符号*
+     */
+    public static final String STR_STAR = "*";
+    
+    /**
+     * 符号\\
+     */
+    public static final String STR_BACK_SLASH = "\\";
+    
+    /**
+     * 符号/
+     */
+    public static final String STR_SLASH = "/";
+    
+    /**
+     * 符号?
+     */
+    public static final String STR_QUESTION = "?";
+    
+    /**
+     * 符号&
+     */
+    public static final String STR_AND = "&";
+    
+    /**
+     * 字符串true
+     */
+    public static final String STR_TRUE = "true";
+    
+    /**
+     * 字符串false
+     */
+    public static final String STR_FALSE = "false";
+    
+    /**
+     * 字符串"unknown"
+     */
+    public static final String STR_UNKNOWN = "unknown";
+    
+    /**
+     * 字符串"null"
+     */
+    public static final String STR_NULL = "null";
+    
+    /**
+     * 字符串"exception"
+     */
+    public static final String STR_EXCEPTION = "exception";
+    
+    /**
+     * 字符串"linux"
+     */
+    public static final String OS_LINUX = "linux";
+    
+    /**
+     * 字符串"windows"
+     */
+    public static final String OS_WIN = "windows";
+    
+    /**
+     * 字符\
+     */
+    public static final char CHAR_BACK_SLASH = '\\';
+    
+    /**
+     * 字符-
+     */
+    public static final char CHAR_MINUS = '-';
+    
+    /**
+     * SQL关键字-"select"
+     */
+    public static final String SQL_KEY_SELECT= "select";
+    
+    /**
+     * SQL关键字-"select distinct"
+     */
+    public static final String SQL_KEY_SELECT_DIST= "select distinct";
+    
+    /**
+     * SQL方法名-执行查询.
+     */
+    public static final String SQL_FUNC_QUERY= "executeQuery";
+    
+    /**
+     * SQL方法名-设置为Null.
+     */
+    public static final String SQL_FUNC_SETNULL= "setNull";
+    
+    /**
+     * SQL方法名-获得结果集.
+     */
+    public static final String SQL_FUNC_GET_RESULTSET= "getResultSet";
+    
+    /**
+     * SQL关键字metaParameters
+     */
+    public static final String SQL_WORD_PARAMS = "metaParameters";
+    
+    /**
+     * SQL方法名-获得更新计数.
+     */
+    public static final String SQL_FUNC_GET_UPDATE_COUNT= "getUpdateCount";
+    
+    /**
+     * 配置常量-日志是否显示SQL参数
+     */
+    public static final String CONF_SHOW_SQL_PARAMS_1 = "1";
+    
+    /**
+     * 配置常量-是否允许多账号同时登录
+     */
+    public static final String CONF_MULTIACCOUNT_LOGIN = "user.multiAccountLogin";
+    
+    /**
+     * 配置常量-初始密码
+     */
+    public static final String CONF_DEFAULT_PASS4USER = "defaultPass4user";
+    
+    /**
+     * 配置常量-'WEB视图前缀
+     */
+    public static final String CONF_VIEW_PREFIX = "web.view.prefix";
+    
+    /**
+     * 配置常量-WEB视图后缀
+     */
+    public static final String CONF_VIEW_SUFFIX = "web.view.suffix";
+    
+    /**
+     * 配置常量-是否允许刷新首页
+     */
+    public static final String CONF_NOT_ALLOW_REFRESH_INDEX = "notAllowRefreshIndex";
+    
+    /**
+     * 配置常量-#系统是否跳过crsf检查  true：跳过 | false 需要crsf检查
+     */
+    public static final String CONF_NO_CRSF_CHK = "noCrsfCheck";
+    
+    /**
+     * nginx集群配置，反向代理名(与nginx.conf中proxy_pass(upstream)配置部分)保持一致
+     */
+    public static final String NGINX_PROXY_PASS = "nginx_proxy_pass";
+    
+    /**
+     * 请求头串
+     */
+    public static final String XMLREQ_HEAD = "XMLHttpRequest";
+    
+    /**
+     * 请求头cookie
+     */
+    public static final String REQ_HEAD_COOKIE = "__cookie";
+    
+    /**
+     * 请求头Range
+     */
+    public static final String REQ_HEAD_RANGE = "Range";
+    
+    /**
+     * 请求头Range
+     */
+    public static final String REQ_XML_HTTP_REQ = "XMLHttpRequest";
+    
+    /**
+     * 请求方法POST
+     */
+    public static final String REQ_POST = "POST";
+    
+    /**
+     * 请求方法GET
+     */
+    public static final String REQ_GET = "GET";
+    
+    /**
+     * 本机请求IP "0:0:0:0:0:0:0:1".
+     */
+    public static final String REQ_LOCAL_IP = "0:0:0:0:0:0:0:1";
+    
+    /**
+     * 异常信息前缀
+     */
+    public static final String PREF4EX_MSG = "msg:";
+    
+    /**
+     * JAVA关键词-java包前缀
+     */
+    public static final String PREF_JAVA = "java.";
+    
+    /**
+     * JAVA关键词-javax包前缀
+     */
+    public static final String PREF_JAVAX = "javax.";
+    
+    /**
+     * 参数正则类型-INT
+     */
+    public static final String REG_NAME_INT = "int";
+    
+    /**
+     * 参数正则类型-yesorno
+     */
+    public static final String REG_NAME_YES_NO = "yesorno";
+    
+    /**
+     * 参数正则类型-year
+     */
+    public static final String REG_NAME_YEAR = "year";
+    
+    /**
+     * 参数正则类型-date
+     */
+    public static final String REG_NAME_DATE = "date";
+    
+    /**
+     * 参数正则类型-url
+     */
+    public static final String REG_NAME_URL = "url";
+    
+    /**
+     * 数据库类型-"oracle"
+     */
+    public static final String DB_TYPE_ORA = "oracle";
+    
+    /**
+     * SSO模式1--引入整体框架
+     */
+    public static final String SSO_MODE_ALL = "1";
+    
+    /**
+     * SSO模式0--引入指定页面
+     */
+    public static final String SSO_MODE_PAGE = "0";
+    
+}
+
---
+++ jflow-core/src/main/java/BP/Tools/Constant4File.java
@@ -0,0 +1,355 @@
+/**
+ * Project Name:app-engine
+ * File Name:Constant4Comm.java
+ * Package Name:com.bwda.engine.common
+ * Date:2017年10月19日
+ * Copyright (c) 2017, 江苏保旺达软件有限公司 All Rights Reserved.
+ *
+ */
+package BP.Tools;
+
+/**
+ * 共通常量定义
+ *
+ * @author fanchengliang
+ * @date 2017年10月19日 
+ */
+public class Constant4File {
+
+    /**
+     * 文件类型后缀-xls
+     */
+    public static final String SUFIX_XLS = "xls";
+    
+    /**
+     * 文件类型后缀-xlsx
+     */
+    public static final String SUFIX_XLSX = "xlsx";
+    
+    /**
+     * 文件类型后缀-apk
+     */
+    public static final String SUFIX_APK = "apk";
+    
+    /**
+     * 文件类型后缀-sis
+     */
+    public static final String SUFIX_SIS = "sis";
+    
+    /**
+     * 文件类型后缀-sisx
+     */
+    public static final String SUFIX_SISX = "sisx";
+    
+    /**
+     * 文件类型后缀-exe
+     */
+    public static final String SUFIX_EXE = "exe";
+    
+    /**
+     * 文件类型后缀-msi
+     */
+    public static final String SUFIX_MSI = "msi";
+    
+    /**
+     * 文件类型后缀-"css"
+     */
+    public static final String SUFIX_CSS = "css";
+    
+    /**
+     * 文件类型后缀-"xml"
+     */
+    public static final String SUFIX_XML = "xml";
+    
+    /**
+     * 文件类型后缀-"gif"
+     */
+    public static final String SUFIX_GIF = "gif";
+    
+    /**
+     * 文件类型后缀-"jpeg"
+     */
+    public static final String SUFIX_JPEG = "jpeg";
+    
+    /**
+     * 文件类型后缀-"jpg"
+     */
+    public static final String SUFIX_JPG = "jpg";
+    
+    /**
+     * 文件类型后缀-"js"
+     */
+    public static final String SUFIX_JS = "js";
+    
+    /**
+     * 文件类型后缀-"atom"
+     */
+    public static final String SUFIX_ATOM = "atom";
+    
+    /**
+     * 文件类型后缀-"rss"
+     */
+    public static final String SUFIX_RSS = "rss";
+    
+    /**
+     * 文件类型后缀-"mml"
+     */
+    public static final String SUFIX_MML = "mml";
+    
+    /**
+     * 文件类型后缀-"txt"
+     */
+    public static final String SUFIX_TXT = "txt";
+    
+    /**
+     * 文件类型后缀-"jad"
+     */
+    public static final String SUFIX_JAD = "jad";
+    
+    /**
+     * 文件类型后缀-"wml"
+     */
+    public static final String SUFIX_WML = "wml";
+    
+    /**
+     * 文件类型后缀-"htc"
+     */
+    public static final String SUFIX_HTC = "htc";
+    
+    /**
+     * 文件类型后缀-"png"
+     */
+    public static final String SUFIX_PNG = "png";
+    
+    /**
+     * 文件类型后缀-"tif"
+     */
+    public static final String SUFIX_TIF = "tif";
+    
+    /**
+     * 文件类型后缀-"tiff"
+     */
+    public static final String SUFIX_TIFF = "tiff";
+    
+    /**
+     * 文件类型后缀-"wbmp"
+     */
+    public static final String SUFIX_WBMP = "wbmp";
+    
+    /**
+     * 文件类型后缀-"ico"
+     */
+    public static final String SUFIX_ICO = "ico";
+    
+    /**
+     * 文件类型后缀-"jng"
+     */
+    public static final String SUFIX_JNG = "jng";
+    
+    /**
+     * 文件类型后缀-"bmp"
+     */
+    public static final String SUFIX_BMP = "bmp";
+    
+    /**
+     * 文件类型后缀-"svg"
+     */
+    public static final String SUFIX_SVG = "svg";
+    
+    /**
+     * 文件类型后缀-"jar"
+     */
+    public static final String SUFIX_JAR = "jar";
+    
+    /**
+     * 文件类型后缀-"var"
+     */
+    public static final String SUFIX_VAR = "var";
+    
+    /**
+     * 文件类型后缀-"ear"
+     */
+    public static final String SUFIX_EAR = "ear";
+    
+    /**
+     * 文件类型后缀-"doc"
+     */
+    public static final String SUFIX_DOC = "doc";
+    
+    /**
+     * 文件类型后缀-"pdf"
+     */
+    public static final String SUFIX_PDF = "pdf";
+    
+    /**
+     * 文件类型后缀-"rtf"
+     */
+    public static final String SUFIX_RTF = "rtf";
+    
+    /**
+     * 文件类型后缀-"ppt"
+     */
+    public static final String SUFIX_PPT = "ppt";
+    
+    /**
+     * 文件类型后缀-"7z"
+     */
+    public static final String SUFIX_7Z = "7z";
+    
+    /**
+     * 文件类型后缀-"rar"
+     */
+    public static final String SUFIX_RAR = "rar";
+    
+    /**
+     * 文件类型后缀-"swf"
+     */
+    public static final String SUFIX_SWF = "swf";
+    
+    /**
+     * 文件类型后缀-"rpm"
+     */
+    public static final String SUFIX_RPM = "rpm";
+    
+    /**
+     * 文件类型后缀-"der"
+     */
+    public static final String SUFIX_DER = "der";
+    
+    /**
+     * 文件类型后缀-"pem"
+     */
+    public static final String SUFIX_PEM = "pem";
+    
+    /**
+     * 文件类型后缀-"crt"
+     */
+    public static final String SUFIX_CRT = "crt";
+    
+    /**
+     * 文件类型后缀-"html"
+     */
+    public static final String SUFIX_HTML = "html";
+    
+    /**
+     * 文件类型后缀-"html"
+     */
+    public static final String SUFIX_HTM = "htm";
+    
+    /**
+     * 文件类型后缀-"shtml"
+     */
+    public static final String SUFIX_SHTML = "shtml";
+    
+    /**
+     * 文件类型后缀-"xhtml"
+     */
+    public static final String SUFIX_XHTML = "xhtml";
+    
+    /**
+     * 文件类型后缀-"zip"
+     */
+    public static final String SUFIX_ZIP = "zip";
+    
+    /**
+     * 文件类型后缀-"mid"
+     */
+    public static final String SUFIX_MID = "mid";
+    
+    /**
+     * 文件类型后缀-"midi"
+     */
+    public static final String SUFIX_MIDI = "midi";
+    
+    /**
+     * 文件类型后缀-"kar"
+     */
+    public static final String SUFIX_KAR = "kar";
+    
+    /**
+     * 文件类型后缀-"mp3"
+     */
+    public static final String SUFIX_MP3 = "mp3";
+    
+    /**
+     * 文件类型后缀-"ogg"
+     */
+    public static final String SUFIX_OGG = "ogg";
+    
+    /**
+     * 文件类型后缀-"m4a"
+     */
+    public static final String SUFIX_M4A = "m4a";
+    
+    /**
+     * 文件类型后缀-"ra"
+     */
+    public static final String SUFIX_RA = "ra";
+    
+    /**
+     * 文件类型后缀-"3gpp"
+     */
+    public static final String SUFIX_3GPP = "3gpp";
+    
+    /**
+     * 文件类型后缀-"3gp"
+     */
+    public static final String SUFIX_3GP = "3gp";
+    
+    /**
+     * 文件类型后缀-"mp4"
+     */
+    public static final String SUFIX_MP4 = "mp4";
+    
+    /**
+     * 文件类型后缀-"mpg"
+     */
+    public static final String SUFIX_MPG = "mpg";
+    
+    /**
+     * 文件类型后缀-"mpeg"
+     */
+    public static final String SUFIX_MPEG = "mpeg";
+    
+    /**
+     * 文件类型后缀-"mov"
+     */
+    public static final String SUFIX_MOV = "mov";
+    
+    /**
+     * 文件类型后缀-"flv"
+     */
+    public static final String SUFIX_FLV = "flv";
+    
+    /**
+     * 文件类型后缀-"m4v"
+     */
+    public static final String SUFIX_M4V = "m4v";
+    
+    /**
+     * 文件类型后缀-"mng"
+     */
+    public static final String SUFIX_MNG = "mng";
+    
+    /**
+     * 文件类型后缀-"asx"
+     */
+    public static final String SUFIX_ASX = "asx";
+    
+    /**
+     * 文件类型后缀-"asf"
+     */
+    public static final String SUFIX_ASF = "asf";
+    
+    /**
+     * 文件类型后缀-"wmv"
+     */
+    public static final String SUFIX_WMV = "wmv";
+    
+    /**
+     * 文件类型后缀-"avi"
+     */
+    public static final String SUFIX_AVI = "avi";
+    
+}
+

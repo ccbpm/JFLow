3274545546bdf50f666ffcce11065e2f3072d3ed
ccflow..liyan <3301790953@qq.com>
2018-06-20 17:25:54 +0800

    no commit message

--- jflow-web/src/main/webapp/WF/Data/UpdataCCFlowVer.sql
+++ jflow-web/src/main/webapp/WF/Data/UpdataCCFlowVer.sql
@@ -1,10 +1,18 @@
 ﻿
 /** -- ========================= 系统升级SQL (为了方便系统升级代码写入的问题,增加该SQL) 目的是为了方便JFlow CCFlow 的统一版本升级. **/
+
+
+update sys_groupfield set frmID=enName where frmid is  null;
+
+UPDATE Sys_MapData SET FK_FormTree='' WHERE No LIKE 'ND%';
+
 DELETE FROM Sys_Enum WHERE EnumKey ='SelectorModel';
+DELETE FROM Sys_Enum WHERE EnumKey ='SrcType';
 DELETE FROM Sys_Enum WHERE EnumKey ='CondModel';
-DELETE FROM Sys_Enum WHERE EnumKey ='FrmTrackSta';
-
-
+DELETE FROM Sys_Enum WHERE EnumKey ='FrmTrackSta'; 
+DELETE FROM Sys_Enum WHERE EnumKey ='EventDoType'; 
+DELETE FROM Sys_Enum WHERE EnumKey ='EditModel';
+DELETE FROM Sys_Enum WHERE EnumKey ='AthRunModel'; 
 -- 更新枚举值;
 DELETE FROM Sys_Enum WHERE EnumKey ='CodeStruct';
 DELETE FROM Sys_Enum WHERE EnumKey ='DBSrcType';
@@ -29,8 +37,6 @@ DELETE FROM Sys_Enum WHERE EnumKey ='FTCSta';
 DELETE FROM Sys_Enum WHERE EnumKey ='SrcType';
 DELETE FROM Sys_Enum WHERE EnumKey IN ('TodolistModel','CCStaWay','TWay' );
 
-
-
 DELETE FROM Sys_GloVar WHERE GroupKey='DefVal';
 
 INSERT INTO Sys_GloVar (No,Name,GroupKey) VALUES ('0','选择系统约定默认值','DefVal');
@@ -46,12 +52,14 @@ INSERT INTO Sys_GloVar (No,Name,GroupKey) VALUES ('@yy年MM月dd日HH时mm分','
 INSERT INTO Sys_GloVar (No,Name,GroupKey) VALUES ('@FK_ND','当前年度','DefVal');
 INSERT INTO Sys_GloVar (No,Name,GroupKey) VALUES ('@FK_YF','当前月份','DefVal');
 INSERT INTO Sys_GloVar (No,Name,GroupKey) VALUES ('@CurrWorker','当前工作可处理人员','DefVal');
-
-
-
+ 
 -- 升级数据源 2016.0
 UPDATE Sys_SFTable SET SrcType=0 WHERE No LIKE '%.%';
 UPDATE Sys_SFTable SET SrcType=1 WHERE No NOT LIKE '%.%' AND SrcType=0;
+
+--更新日期长度.
+UPDATE SYS_MAPATTR SET UIWidth=125 WHERE MYDATATYPE=6
+UPDATE SYS_MAPATTR SET UIWidth=145 WHERE MYDATATYPE=7
 
 
 -- 2016.11.18 升级维护附件属性.;
@@ -59,19 +67,18 @@ DELETE FROM Sys_EnCfg WHERE No='BP.Sys.FrmUI.FrmAttachmentExt';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.Sys.FrmUI.FrmAttachmentExt',
 '@MyPK=基础信息,附件的基本配置.
 @DeleteWay=权限控制,控制附件的下载与上传权限.@IsRowLock=WebOffice属性,设置与公文有关系的属性配置.
-@IsToHeLiuHZ=流程相关,控制节点附件的分合流.
-@FastKeyIsEnable=快捷键,为附件生成快捷键放入指定的目录.');
+@IsToHeLiuHZ=流程相关,控制节点附件的分合流.');
 
 -- 2016.07.20 升级明细表维护分组;
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.MapDtlExt';
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapDtlExt','@No=基础信息,基础信息权限信息.@IsExp=数据导入导出,数据导入导出.@MTR=多表头,实现多表头.@FrmThreadLab=超连接,对合流节点有效用于配置与现实子线程运行的情况。@FrmTrackLab=流程相关,用于显示流程运行的轨迹图.');
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapDtlExt','@No=基础信息,基础信息权限信息.@IsExp=数据导入导出,数据导入导出.@MTR=多表头,实现多表头.@IsEnableLink=超链接,显示在从表的右边.@IsCopyNDData=流程相关,与流程相关的配置非流程可以忽略.');
 
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.FrmNodeComponent';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.FrmNodeComponent','@NodeID=审核组件,适用于sdk表单审核组件与ccform上的审核组件属性设置.@SFLab=父子流程组件,在该节点上配置与显示父子流程.@FrmThreadLab=子线程组件,对合流节点有效，用于配置与现实子线程运行的情况。@FrmTrackLab=轨迹组件,用于显示流程运行的轨迹图.@FTCLab=流转自定义,在每个节点上自己控制节点的处理人.');
 
 -- 傻瓜表单属性;
-DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.MapFoolForm';
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapFoolForm','@No=基础属性,基础属性.@Designer=设计者信息,设计者的单位信息，人员信息，可以上传到表单云.');
+DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.MapFrmFool';
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapFrmFool','@No=基础属性,基础属性.@Designer=设计者信息,设计者的单位信息，人员信息，可以上传到表单云.');
 
 --新版本的流程属性,节点属性;
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.NodeExt';
@@ -92,7 +99,7 @@ DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.NodeSheet';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.NodeSheet','@NodeID=基本配置@FormType=表单@FWCSta=审核组件,适用于sdk表单审核组件与ccform上的审核组件属性设置.@SFSta=父子流程,对启动，查看父子流程的控件设置.@SendLab=按钮权限,控制工作节点可操作按钮.@RunModel=运行模式,分合流,父子流程@AutoJumpRole0=跳转,自动跳转规则当遇到该节点时如何让其自动的执行下一步.@MPhone_WorkModel=移动,与手机平板电脑相关的应用设置.@OfficeOpen=公文按钮,只有当该节点是公文流程时候有效');
  
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.FlowSheet';                 
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.FlowSheet','@No=基本配置@FlowRunWay=启动方式,配置工作流程如何自动发起，该选项要与流程服务一起工作才有效.@StartLimitRole=启动限制规则@StartGuideWay=发起前置导航@DTSWay=流程数据与业务数据同步@PStarter=轨迹查看权限');
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.FlowSheet','@No=基本配置@FlowRunWay=启动方式,配置工作流程如何自动发起，该选项要与流程服务一起工作才有效.@StartLimitRole=启动限制规则@StartGuideWay=发起前置导航@CFlowWay=延续流程@DTSWay=流程数据与业务数据同步@PStarter=轨迹查看权限');
 
 
 

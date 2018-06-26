b54716214f23fe6aa62ae8a9484a0543520696c6
jflow杜栋梁 <dudongliang@ccflow.org>
2018-06-26 16:23:44 +0800

    添加HttpServlet的两个参数,用于上传下载

--- jflow-core/src/main/java/cn/jflow/controller/wf/admin/AttrNodeController.java
+++ jflow-core/src/main/java/cn/jflow/controller/wf/admin/AttrNodeController.java
@@ -1,8 +1,12 @@
 package cn.jflow.controller.wf.admin;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 
 import BP.WF.HttpHandler.WF_Admin_AttrFlow;
 import BP.WF.HttpHandler.WF_Admin_AttrNode;
@@ -18,9 +22,13 @@ public class AttrNodeController extends HttpHandlerBase{
 	 * @return
 	 */
 	@RequestMapping(value = "/ProcessRequest")
-	public final void ProcessRequestPost() 
+	public final void ProcessRequestPost(HttpServletRequest request , HttpServletResponse response) 
 	{
 		WF_Admin_AttrNode  AttrHandler = new WF_Admin_AttrNode();
+		AttrHandler.setHttpServletResp(response);
+		if (request instanceof DefaultMultipartHttpServletRequest) {
+			AttrHandler.setMultipartRequest((DefaultMultipartHttpServletRequest) request);
+		}
 		super.ProcessRequest(AttrHandler);
 	}
 	@Override

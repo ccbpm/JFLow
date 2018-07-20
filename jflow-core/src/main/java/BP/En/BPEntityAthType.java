76c05818a167e5fdf69bc7bad6e4d14dcc6e65db
樊雷伟 <2057946457@qq.com>
2018-07-20 16:18:54 +0800

    no commit message

---
+++ jflow-core/src/main/java/BP/En/BPEntityAthType.java
@@ -0,0 +1,16 @@
+package BP.En;
+
+public enum BPEntityAthType {
+	/// <summary>
+    /// 无
+    /// </summary>
+    None,
+    /// <summary>
+    /// 单附件
+    /// </summary>
+    Single,
+    /// <summary>
+    /// 多附件
+    /// </summary>
+    Multi
+}

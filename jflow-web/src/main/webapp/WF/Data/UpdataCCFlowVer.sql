275a99fbbbf6f8e7b4f31799f722fb6a3206c648
周朋 <hiflow@qq.com>
2018-07-27 17:59:09 +0800

    no commit message

--- jflow-web/src/main/webapp/WF/Data/UpdataCCFlowVer.sql
+++ jflow-web/src/main/webapp/WF/Data/UpdataCCFlowVer.sql
@@ -1,7 +1,5 @@
 ﻿
 /** -- ========================= 系统升级SQL (为了方便系统升级代码写入的问题,增加该SQL) 目的是为了方便JFlow CCFlow 的统一版本升级. **/
-
-
 update sys_groupfield set frmID=enName where frmid is  null;
 
 UPDATE Sys_MapData SET FK_FormTree='' WHERE No LIKE 'ND%';
@@ -61,6 +59,10 @@ UPDATE Sys_SFTable SET SrcType=1 WHERE No NOT LIKE '%.%' AND SrcType=0;
 UPDATE SYS_MAPATTR SET UIWidth=125 WHERE MYDATATYPE=6
 UPDATE SYS_MAPATTR SET UIWidth=145 WHERE MYDATATYPE=7
 
+-- 2018.07.24 ; 
+DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.FlowExt';
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.FlowExt','@No=基础信息,基础信息权限信息.@IsBatchStart=数据&表单,数据导入导出.@DesignerNo=设计者,流程开发设计者信息');
+
 
 -- 2016.11.18 升级维护附件属性.;
 DELETE FROM Sys_EnCfg WHERE No='BP.Sys.FrmUI.FrmAttachmentExt';
@@ -73,26 +75,21 @@ INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.Sys.FrmUI.FrmAttachmentExt',
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.MapDtlExt';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapDtlExt','@No=基础信息,基础信息权限信息.@IsExp=数据导入导出,数据导入导出.@MTR=多表头,实现多表头.@IsEnableLink=超链接,显示在从表的右边.@IsCopyNDData=流程相关,与流程相关的配置非流程可以忽略.');
 
+
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.FrmNodeComponent';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.FrmNodeComponent','@NodeID=审核组件,适用于sdk表单审核组件与ccform上的审核组件属性设置.@SFLab=父子流程组件,在该节点上配置与显示父子流程.@FrmThreadLab=子线程组件,对合流节点有效，用于配置与现实子线程运行的情况。@FrmTrackLab=轨迹组件,用于显示流程运行的轨迹图.@FTCLab=流转自定义,在每个节点上自己控制节点的处理人.');
 
--- 傻瓜表单属性;
+-- 傻瓜表单属性; 
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.MapFrmFool';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapFrmFool','@No=基础属性,基础属性.@Designer=设计者信息,设计者的单位信息，人员信息，可以上传到表单云.');
 
 --新版本的流程属性,节点属性;
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.NodeExt';
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.NodeExt','@NodeID=基本配置@FWCSta=审核组件,适用于sdk表单审核组件与ccform上的审核组件属性设置.@SendLab=按钮权限,控制工作节点可操作按钮.@RunModel=运行模式,分合流,父子流程@AutoJumpRole0=跳转,自动跳转规则当遇到该节点时如何让其自动的执行下一步.@MPhone_WorkModel=移动,与手机平板电脑相关的应用设置.');
-
-DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.FlowExt';
-INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.FlowExt','@No=基本配置');
- 
+INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.NodeExt','@NodeID=基本配置@SendLab=按钮权限,控制工作节点可操作按钮.@RunModel=运行模式,分合流,父子流程@AutoJumpRole0=跳转,自动跳转规则当遇到该节点时如何让其自动的执行下一步.');
+  
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.MapDataExt';
 INSERT INTO Sys_EnCfg(No,GroupTitle) VALUES ('BP.WF.Template.MapDataExt','@No=基本属性@Designer=设计者信息');
 UPDATE Sys_MapData SET AppType=0 WHERE No NOT LIKE 'ND%';
-
-  
-
 
 -- 旧版本的流程属性;
 DELETE FROM Sys_EnCfg WHERE No='BP.WF.Template.NodeSheet';

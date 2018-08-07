b9ad876a4b62e3ef31a2e2ce8b8dc57d7b5d4a76
袁丽娜 <nayuanli@126.com>
2018-08-07 18:50:48 +0800

    no commit message

--- jflow-core/src/main/java/BP/GPM/Emp.java
+++ jflow-core/src/main/java/BP/GPM/Emp.java
@@ -9,6 +9,7 @@ import BP.En.Entities;
 import BP.En.EntityNoName;
 import BP.En.Map;
 import BP.En.RefMethod;
+import BP.En.RefMethodType;
 import BP.En.UAC;
 import BP.Tools.StringHelper;
 
@@ -32,39 +33,34 @@ public class Emp extends EntityNoName
 			return true;
 		}
 
-		if (BP.Sys.SystemConfig.getOSModel() == BP.Sys.OSModel.OneMore)
+		
+		String sql = "SELECT COUNT(FK_Emp) FROM Port_DeptEmpStation WHERE FK_Emp='" + this.getNo() + "'";
+		if (DBAccess.RunSQLReturnValInt(sql, 0) == 0)
 		{
-			String sql = "SELECT COUNT(FK_Emp) FROM Port_DeptEmpStation WHERE FK_Emp='" + this.getNo() + "'";
-			if (DBAccess.RunSQLReturnValInt(sql, 0) == 0)
-			{
-				return false;
-			}
-
-			sql = "SELECT COUNT(FK_Emp) FROM Port_DeptEmp WHERE FK_Emp='" + this.getNo() + "'";
-			if (DBAccess.RunSQLReturnValInt(sql, 0) == 0)
-			{
-				return false;
-			}
+			return false;
 		}
 
-		if (BP.Sys.SystemConfig.getOSModel() == BP.Sys.OSModel.OneOne)
+		sql = "SELECT COUNT(FK_Emp) FROM Port_DeptEmp WHERE FK_Emp='" + this.getNo() + "'";
+		if (DBAccess.RunSQLReturnValInt(sql, 0) == 0)
 		{
-			String sql = "SELECT COUNT(FK_Emp) FROM Port_EmpStation WHERE FK_Emp='" + this.getNo() + "'";
-			if (DBAccess.RunSQLReturnValInt(sql, 0) == 0)
-			{
-				return false;
-			}
-
-			sql = "SELECT COUNT(FK_Emp) FROM Port_DeptEmp WHERE FK_Emp='" + this.getNo() + "'";
-			if (DBAccess.RunSQLReturnValInt(sql, 0) == 0)
-			{
-				return false;
-			}
+			return false;
 		}
-
+		
+	
 		return true;
 	}
-	 
+	
+	/**
+	 * 拼音
+	 * @return
+	 */
+	public String getPinYin(){
+		return this.GetValStrByKey(EmpAttr.PinYin);
+	}
+	public void setPinYin(String value){
+		this.SetValByKey(EmpAttr.PinYin, value);
+	}
+	
 	/** 
 	 主要的部门。
 	 * @throws Exception 
@@ -100,7 +96,7 @@ public class Emp extends EntityNoName
 		return this.GetValRefTextByKey(EmpAttr.FK_Dept);
 	}
 	 
-
+	
 	public final String getLeader()
 	{
 		return this.GetValStrByKey(DeptEmpAttr.Leader);
@@ -109,6 +105,29 @@ public class Emp extends EntityNoName
 	{
 		this.SetValByKey(DeptEmpAttr.Leader, value);
 	}
+	
+	/** 
+	 Tel
+	 
+	*/
+	public String getTel(){
+		return this.GetValStrByKey(EmpAttr.Tel);
+	}
+	public void setTel(String value){
+		this.SetValByKey(EmpAttr.Tel, value);
+	}
+	
+	/** 
+	 Email
+	 
+	*/
+	public String getEmail(){
+		return this.GetValStrByKey(EmpAttr.Email);
+	}
+	public void setEmail(String value){
+		this.SetValByKey(EmpAttr.Email, value);
+	}
+	
 	/** 
 	 密码
 	 
@@ -132,6 +151,17 @@ public class Emp extends EntityNoName
 	public final void setIdx(int value)
 	{
 		this.SetValByKey(EmpAttr.Idx, value);
+	}
+	
+	/**
+	 签字类型
+	 */
+	public int getSignType(){
+		return this.GetValIntByKey(EmpAttr.SignType);
+	}
+	
+	public void setSignType(int value){
+		this.SetValByKey(EmpAttr.SignType, value);
 	}
 	/** 
 	 检查密码(可以重写此方法)
@@ -203,39 +233,49 @@ public class Emp extends EntityNoName
 
 		map.setEnDBUrl(new DBUrl(DBUrlType.AppCenterDSN)); //要连接的数据源（表示要连接到的那个系统数据库）。
 		map.setPhysicsTable("Port_Emp"); // 要物理表。
-		//map.Java_SetDepositaryOfMap(Depositary.Application); //实体map的存放位置.
-	//	map.Java_SetDepositaryOfEntity(Depositary.Application); //实体存放位置
+		map.Java_SetDepositaryOfMap(Depositary.Application); //实体map的存放位置.
+		map.Java_SetDepositaryOfEntity(Depositary.Application); //实体存放位置
 		map.setEnDesc("用户"); // "用户"; // 实体的描述.
 		map.Java_SetEnType(EnType.App); //实体类型。
 
-			//关于字段属性的增加 
-		map.AddTBStringPK(EmpAttr.No, null, "编号", true, false, 1, 50, 50);
-		 
-		map.AddTBString(EmpAttr.Name, null, "名称", true, false, 0, 200, 30);
-		map.AddTBString(EmpAttr.Pass, "123", "密码", false, false, 0, 100, 10);
+	     /*关于字段属性的增加 */
+        map.AddTBStringPK(EmpAttr.No, null, "登陆账号", true, false, 1, 50, 50);
+        map.AddTBString(EmpAttr.Name, null, "名称", true, false, 0, 200, 30);
+        map.AddTBString(EmpAttr.Pass, "123", "密码", false, false, 0, 100, 10);
 
-			//map.AddDDLEntities(EmpAttr.FK_Dept, null, "部门", new Port.Depts(), true);
+        map.AddDDLEntities(EmpAttr.FK_Dept, null, "主要部门", new BP.Port.Depts(), true);
 
-		map.AddTBString(EmpAttr.FK_Dept, null, "当前部门", false, false, 0, 50, 50);
-		 
+        map.AddTBString(EmpAttr.SID, null, "安全校验码", false, false, 0, 36, 36);
+        map.AddTBString(EmpAttr.Tel, null, "电话", true, false, 0, 20, 130);
+        map.AddTBString(EmpAttr.Email, null, "邮箱", true, false, 0, 100, 132, true);
+        map.AddTBString(EmpAttr.PinYin, null, "拼音", true, false, 0, 500, 132, true);
 
-		map.AddTBString(EmpAttr.SID, null, "安全校验码", false, false, 0, 36, 36);
-		map.AddTBString(EmpAttr.Tel, null, "电话", true, false, 0, 20, 130);
-		map.AddTBString(EmpAttr.Email, null, "邮箱", true, false, 0, 100, 132);
- 
-		map.AddTBInt(EmpAttr.Idx, 0, "序号", true, false);
-	//	 map.AddSearchAttr(EmpAttr.FK_Dept);
+        // 0=不签名 1=图片签名, 2=电子签名.
+        map.AddDDLSysEnum(EmpAttr.SignType, 0, "签字类型", true, true, EmpAttr.SignType,
+            "@0=不签名@1=图片签名@2=电子签名");
 
-			////#region 增加点对多属性
-			////他的部门权限
-			//map.AttrsOfOneVSM.Add(new EmpDepts(), new Depts(), EmpDeptAttr.FK_Emp, EmpDeptAttr.FK_Dept,
-			//    DeptAttr.Name, DeptAttr.No, "部门权限");
+        map.AddTBInt(EmpAttr.Idx, 0, "序号", true, false);
+        ///#endregion 字段
 
-			//map.AttrsOfOneVSM.Add(new EmpStations(), new Stations(), EmpStationAttr.FK_Emp, EmpStationAttr.FK_Station,
-			//    DeptAttr.Name, DeptAttr.No, "岗位权限");
-			////#endregion
+        map.AddSearchAttr(EmpAttr.SignType);
 
-		RefMethod rm = new RefMethod();
+         RefMethod rm = new RefMethod();
+         rm.Title = "设置图片签名";
+         rm.ClassMethodName = this.toString() + ".DoSinger";
+         rm.refMethodType = RefMethodType.RightFrameOpen;
+         map.AddRefMethod(rm);
+
+         rm = new RefMethod();
+         rm.Title = "部门岗位";
+         rm.ClassMethodName = this.toString() + ".DoEmpDepts";
+         rm.refMethodType = RefMethodType.RightFrameOpen;
+         map.AddRefMethod(rm);
+
+         //节点绑定部门. 节点绑定部门.
+         map.getAttrsOfOneVSM().AddBranches(new DeptEmps(), new BP.GPM.Depts(),
+            BP.GPM.DeptEmpAttr.FK_Emp,
+            BP.GPM.DeptEmpAttr.FK_Dept, "部门维护", EmpAttr.Name, EmpAttr.No, "@WebUser.FK_Dept");
+
 		rm.Title = "修改密码";
 		rm.ClassMethodName = this.toString() + ".DoResetpassword";
 		rm.getHisAttrs().AddTBString("pass1", null, "输入密码", true, false, 0, 100, 100);
@@ -245,6 +285,60 @@ public class Emp extends EntityNoName
 		this.set_enMap(map);
 		return this.get_enMap();
 	}
+	
+	 public String DoEmpDepts()
+     {
+         return "../../../GPM/EmpDepts.htm?FK_Emp=" + this.getNo();
+     }
+
+     public String DoSinger()
+     {
+         return "../../../GPM/Siganture.htm?EmpNo=" + this.getNo();
+     }
+
+
+     protected boolean beforeUpdateInsertAction() throws Exception
+     {
+         //增加拼音，以方便查找.
+         String pinyinQP = BP.DA.DataType.ParseStringToPinyin(this.getName()).toLowerCase();
+         String pinyinJX = BP.DA.DataType.ParseStringToPinyinJianXie(this.getName()).toLowerCase();
+         this.setPinYin("," + pinyinQP + "," + pinyinJX + ",");
+
+         //处理岗位信息.
+         DeptEmpStations des = new DeptEmpStations();
+         des.Retrieve(DeptEmpStationAttr.FK_Emp, this.getNo());
+
+         String depts = "";
+         String stas = "";
+
+         for (DeptEmpStation item : des.ToJavaList())
+         {
+             BP.GPM.Dept dept = new BP.GPM.Dept();
+             dept.setNo(item.getFK_Dept());
+             if (dept.RetrieveFromDBSources() == 0)
+             {
+                 item.Delete();
+                 continue;
+             }
+
+             //给拼音重新定义值,让其加上部门的信息.
+             this.setPinYin(this.getPinYin() + pinyinJX + "/" + BP.DA.DataType.ParseStringToPinyinJianXie(dept.getName()).toLowerCase()+",");
+
+             BP.Port.Station sta = new BP.Port.Station();
+             sta.setNo(item.getFK_Station());
+             if (sta.RetrieveFromDBSources() == 0)
+             {
+                 item.Delete();
+                 continue;
+             }
+
+             stas += "@" + dept.getNameOfPath() + "|" + sta.getName();
+             depts += "@" + dept.getNameOfPath();
+         }
+
+
+         return super.beforeUpdateInsertAction();
+     }
 
 	/** 
 	 向上移动

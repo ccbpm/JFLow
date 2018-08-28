ef2e8872fe4eb7f5e932ffac7dfe11da05ec1508
袁丽娜 <nayuanli@126.com>
2018-08-28 10:21:51 +0800

    no commit message

--- jflow-core/src/main/java/BP/WF/DTS/AutoRunStratFlows.java
+++ jflow-core/src/main/java/BP/WF/DTS/AutoRunStratFlows.java
@@ -1,30 +1,33 @@
 package BP.WF.DTS;
 
 import BP.DA.DBAccess;
+import BP.DA.DataColumn;
 import BP.DA.DataRow;
 import BP.DA.DataSet;
 import BP.DA.DataTable;
-import BP.DA.DataType;
-import BP.DA.LogType;
+import BP.En.Attr;
 import BP.En.Method;
-import BP.Port.Emp;
+import BP.Sys.GEDtl;
+import BP.Sys.GEDtlAttr;
+import BP.Sys.MapDtl;
+import BP.Sys.MapDtls;
 import BP.Sys.MapExt;
-import BP.WF.GenerWorkFlowAttr;
-import BP.WF.GenerWorkerList;
-import BP.WF.GenerWorkerListAttr;
-import BP.WF.GenerWorkerLists;
-import BP.WF.Node;
+import BP.Sys.MapExtXmlList;
+import BP.Tools.DateUtils;
+import BP.Tools.StringHelper;
+import BP.WF.Flow;
+import BP.WF.Flows;
+import BP.WF.SendReturnObjs;
+import BP.WF.Work;
 import BP.WF.WorkNode;
-import BP.WF.Template.NodeAttr;
-import BP.WF.Template.OutTimeDeal;
-import cn.jflow.common.util.DateAndTime;
+import BP.Web.WebUser;
 
 public class AutoRunStratFlows extends Method{
 
 	 /// <summary>
     /// 不带有参数的方法
     /// </summary>
-    public void AutoRunStratFlows()
+    public AutoRunStratFlows()
     {
     	  this.Title = "自动启动流程";
           this.Help = "在流程属性上配置的信息,自动发起流程,按照时间规则....";
@@ -49,34 +52,34 @@ public class AutoRunStratFlows extends Method{
     /// <returns>返回执行结果</returns>
     public  Object Do() throws Exception
     {
-    	   BP.WF.Flows fls = new Flows();
+    	   Flows fls = new Flows();
            fls.RetrieveAll();
 
-           #region 自动启动流程
-           foreach (BP.WF.Flow fl in fls)
+           	//#region 自动启动流程
+           for (Flow fl : fls.ToJavaList())
            {
-               if (fl.HisFlowRunWay == BP.WF.FlowRunWay.HandWork)
+               if (fl.getHisFlowRunWay() == BP.WF.FlowRunWay.HandWork)
                    continue;
 
-               if (DateTime.Now.ToString("HH:mm") == fl.Tag)
+               if (DateUtils.getCurrentDate("HH:mm").equals(fl.Tag))
                    continue;
 
-               if (fl.RunObj == null || fl.RunObj == "")
+               if (fl.getRunObj() == null || fl.getRunObj() == "")
                {
-                   string msg = "您设置自动运行流程错误，没有设置流程内容，流程编号：" + fl.No + ",流程名称:" + fl.Name;
+                   String msg = "您设置自动运行流程错误，没有设置流程内容，流程编号：" + fl.getNo() + ",流程名称:" + fl.getName();
                    BP.DA.Log.DebugWriteError(msg);
                    continue;
                }
 
-               #region 判断当前时间是否可以运行它。
-               string nowStr = DateTime.Now.ToString("yyyy-MM-dd,HH:mm");
-               string[] strs = fl.RunObj.Split('@'); //破开时间串。
-               bool IsCanRun = false;
-               foreach (string str in strs)
+               //#region 判断当前时间是否可以运行它。
+               String nowStr = DateUtils.getCurrentDate("yyyy-MM-dd,HH:mm");
+               String[] strs = fl.getRunObj().split("@"); //破开时间串。
+               boolean IsCanRun = false;
+               for (String str : strs)
                {
-                   if (string.IsNullOrEmpty(str))
+                   if (StringHelper.isNullOrEmpty(str))
                        continue;
-                   if (nowStr.Contains(str))
+                   if (nowStr.contains(str))
                        IsCanRun = true;
                }
 
@@ -84,18 +87,18 @@ public class AutoRunStratFlows extends Method{
                    continue;
 
                // 设置时间.
-               fl.Tag = DateTime.Now.ToString("HH:mm");
-               #endregion 判断当前时间是否可以运行它。
+               fl.Tag = DateUtils.getCurrentDate("HH:mm");
+               //#endregion 判断当前时间是否可以运行它。
 
                // 以此用户进入.
-               switch (fl.HisFlowRunWay)
+               switch (fl.getHisFlowRunWay())
                {
-                   case BP.WF.FlowRunWay.SpecEmp: //指定人员按时运行。
-                       string RunObj = fl.RunObj;
-                       string fk_emp = RunObj.Substring(0, RunObj.IndexOf('@'));
+                   case SpecEmp: //指定人员按时运行。
+                       String RunObj = fl.getRunObj();
+                       String fk_emp = RunObj.substring(0, RunObj.indexOf('@'));
 
                        BP.Port.Emp emp = new BP.Port.Emp();
-                       emp.No = fk_emp;
+                       emp.setNo(fk_emp);
                        if (emp.RetrieveFromDBSources() == 0)
                        {
                            BP.DA.Log.DebugWriteError("启动自动启动流程错误：发起人(" + fk_emp + ")不存在。");
@@ -105,24 +108,24 @@ public class AutoRunStratFlows extends Method{
                        try
                        {
                            //让 userNo 登录.
-                           BP.WF.Dev2Interface.Port_Login(emp.No);
+                           BP.WF.Dev2Interface.Port_Login(emp.getNo());
 
                            //创建空白工作, 发起开始节点.
-                           Int64 workID = BP.WF.Dev2Interface.Node_CreateBlankWork(fl.No);
+                           long workID = BP.WF.Dev2Interface.Node_CreateBlankWork(fl.getNo());
 
                            //执行发送.
-                           SendReturnObjs objs = BP.WF.Dev2Interface.Node_SendWork(fl.No, workID);
+                           SendReturnObjs objs = BP.WF.Dev2Interface.Node_SendWork(fl.getNo(), workID);
 
                            //string info_send= BP.WF.Dev2Interface.Node_StartWork(fl.No,);
-                           BP.DA.Log.DefaultLogWriteLineInfo("流程:" + fl.No + fl.Name + "的定时任务\t\n -------------- \t\n" + objs.ToMsgOfText());
+                           BP.DA.Log.DefaultLogWriteLineInfo("流程:" + fl.getNo() + fl.getName() + "的定时任务\t\n -------------- \t\n" + objs.ToMsgOfText());
 
                        }
                        catch (Exception ex)
                        {
-                           BP.DA.Log.DebugWriteError("流程:" + fl.No + fl.Name + "自动发起错误:\t\n -------------- \t\n" + ex.Message);
+                           BP.DA.Log.DebugWriteError("流程:" + fl.getNo() + fl.getName() + "自动发起错误:\t\n -------------- \t\n" + ex.getMessage());
                        }
                        continue;
-                   case BP.WF.FlowRunWay.DataModel: //按数据集合驱动的模式执行。
+                   case DataModel: //按数据集合驱动的模式执行。
                        this.DTS_Flow(fl);
                        continue;
                    default:
@@ -140,85 +143,85 @@ public class AutoRunStratFlows extends Method{
     }
 
 
-    public void DTS_Flow(BP.WF.Flow fl)
+    public void DTS_Flow(BP.WF.Flow fl) throws Exception
     {
       //  #region 读取数据.
         BP.Sys.MapExt me = new MapExt();
-        me.setMyPK( "ND" + int.Parse(fl.No) + "01" + "_" + MapExtXmlList.StartFlow);
+        me.setMyPK( "ND" + Integer.parseInt(fl.getNo()) + "01" + "_" + MapExtXmlList.StartFlow);
         int i = me.RetrieveFromDBSources();
         if (i == 0)
         {
-            BP.DA.Log.DefaultLogWriteLineError("没有为流程(" + fl.Name + ")的开始节点设置发起数据,请参考说明书解决.");
+            BP.DA.Log.DefaultLogWriteLineError("没有为流程(" + fl.getName() + ")的开始节点设置发起数据,请参考说明书解决.");
             return;
         }
-        if (string.IsNullOrEmpty(me.Tag))
+        if (StringHelper.isNullOrEmpty(me.getTag()))
         {
-            BP.DA.Log.DefaultLogWriteLineError("没有为流程(" + fl.Name + ")的开始节点设置发起数据,请参考说明书解决.");
+            BP.DA.Log.DefaultLogWriteLineError("没有为流程(" + fl.getName() + ")的开始节点设置发起数据,请参考说明书解决.");
             return;
         }
 
         // 获取从表数据.
         DataSet ds = new DataSet();
-        string[] dtlSQLs = me.Tag1.Split('*');
-        foreach (string sql in dtlSQLs)
+        String[] dtlSQLs = me.getTag1().split("*");
+        for (String sql : dtlSQLs)
         {
-            if (string.IsNullOrEmpty(sql))
+            if (StringHelper.isNullOrEmpty(sql))
                 continue;
 
-            string[] tempStrs = sql.Split('=');
-            string dtlName = tempStrs[0];
-            DataTable dtlTable = BP.DA.DBAccess.RunSQLReturnTable(sql.Replace(dtlName + "=", ""));
+            String[] tempStrs = sql.split("=");
+            String dtlName = tempStrs[0];
+            DataTable dtlTable = BP.DA.DBAccess.RunSQLReturnTable(sql.replace(dtlName + "=", ""));
             dtlTable.TableName = dtlName;
-            ds.Tables.Add(dtlTable);
+            ds.Tables.add(dtlTable);
         }
-       // #endregion 读取数据.
+        // #endregion 读取数据.
 
-     //   #region 检查数据源是否正确.
-        string errMsg = "";
+        //   #region 检查数据源是否正确.
+        String errMsg = "";
         // 获取主表数据.
-        DataTable dtMain = BP.DA.DBAccess.RunSQLReturnTable(me.Tag);
-        if (dtMain.Rows.Count == 0)
+        DataTable dtMain = BP.DA.DBAccess.RunSQLReturnTable(me.getTag());
+        if (dtMain.Rows.size() == 0)
         {
-            BP.DA.Log.DefaultLogWriteLineError("流程(" + fl.Name + ")此时无任务.");
+            BP.DA.Log.DefaultLogWriteLineError("流程(" + fl.getName() + ")此时无任务.");
             return;
         }
 
-        if (dtMain.Columns.Contains("Starter") == false)
+        if (dtMain.Columns.contains("Starter") == false)
             errMsg += "@配值的主表中没有Starter列.";
 
-        if (dtMain.Columns.Contains("MainPK") == false)
+        if (dtMain.Columns.contains("MainPK") == false)
             errMsg += "@配值的主表中没有MainPK列.";
 
-        if (errMsg.Length > 2)
+        if (errMsg.length() > 2)
         {
-            BP.DA.Log.DefaultLogWriteLineError("流程(" + fl.Name + ")的开始节点设置发起数据,不完整." + errMsg);
+            BP.DA.Log.DefaultLogWriteLineError("流程(" + fl.getName() + ")的开始节点设置发起数据,不完整." + errMsg);
             return;
         }
        // #endregion 检查数据源是否正确.
 
-      //  #region 处理流程发起.
-        string nodeTable = "ND" + int.Parse(fl.No) + "01";
+       //  #region 处理流程发起.
+        String nodeTable = "ND" + Integer.parseInt(fl.getNo()) + "01";
         int idx = 0;
-        foreach (DataRow dr in dtMain.Rows)
+        for (DataRow dr : dtMain.Rows)
         {
             idx++;
 
-            string mainPK = dr["MainPK"].ToString();
-            string sql = "SELECT OID FROM " + nodeTable + " WHERE MainPK='" + mainPK + "'";
-            if (DBAccess.RunSQLReturnTable(sql).Rows.Count != 0)
+            String mainPK = dr.getValue("MainPK").toString();
+            String sql = "SELECT OID FROM " + nodeTable + " WHERE MainPK='" + mainPK + "'";
+            if (DBAccess.RunSQLReturnTable(sql).Rows.size() != 0)
             {
                 continue; /*说明已经调度过了*/
             }
 
-            string starter = dr["Starter"].ToString();
-            if (WebUser.No != starter)
+            String starter = dr.getValue("Starter").toString();
+            if (WebUser.getNo() != starter)
             {
                 BP.Web.WebUser.Exit();
                 BP.Port.Emp emp = new BP.Port.Emp();
-                emp.No = starter;
+                emp.setNo(starter);
                 if (emp.RetrieveFromDBSources() == 0)
                 {
-                    BP.DA.Log.DefaultLogWriteLineInfo("@数据驱动方式发起流程(" + fl.Name + ")设置的发起人员:" + emp.No + "不存在。");
+                    BP.DA.Log.DefaultLogWriteLineInfo("@数据驱动方式发起流程(" + fl.getName() + ")设置的发起人员:" + emp.getNo() + "不存在。");
                     continue;
                 }
                 WebUser.SignInOfGener(emp);
@@ -229,11 +232,11 @@ public class AutoRunStratFlows extends Method{
 
             Work wk = fl.NewWork();
 
-            string err = "";
-            #region 检查用户拼写的sql是否正确？
-            foreach (DataColumn dc in dtMain.Columns)
+            String err = "";
+            //#region 检查用户拼写的sql是否正确？
+            for (DataColumn dc : dtMain.Columns)
             {
-                string f = dc.ColumnName.ToLower();
+                String f = dc.ColumnName.toLowerCase();
                 switch (f)
                 {
                     case "starter":
@@ -242,10 +245,10 @@ public class AutoRunStratFlows extends Method{
                     case "tonode":
                         break;
                     default:
-                        bool isHave = false;
-                        foreach (Attr attr in wk.EnMap.Attrs)
+                        boolean isHave = false;
+                        for (Attr attr : wk.getEnMap().getAttrs())
                         {
-                            if (attr.Key.ToLower() == f)
+                            if (attr.getKey().toLowerCase() == f)
                             {
                                 isHave = true;
                                 break;
@@ -258,41 +261,42 @@ public class AutoRunStratFlows extends Method{
                         break;
                 }
             }
-            if (string.IsNullOrEmpty(err) == false)
-                throw new Exception("您设置的字段:" + err + "不存在开始节点的表单中，设置的sql:" + me.Tag);
+            if (StringHelper.isNullOrEmpty(err) == false)
+                throw new Exception("您设置的字段:" + err + "不存在开始节点的表单中，设置的sql:" + me.getTag());
 
            // #endregion 检查用户拼写的sql是否正确？
 
-            foreach (DataColumn dc in dtMain.Columns)
-                wk.SetValByKey(dc.ColumnName, dr[dc.ColumnName].ToString());
-
-            if (ds.Tables.Count != 0)
+            for (DataColumn dc : dtMain.Columns)
+                wk.SetValByKey(dc.ColumnName, dr.getValue(dc.ColumnName).toString());
+            
+    
+            if (ds.Tables.size() != 0)
             {
                 // MapData md = new MapData(nodeTable);
                 MapDtls dtls = new MapDtls(nodeTable);
-                foreach (MapDtl dtl in dtls)
+                for (MapDtl dtl : dtls.ToJavaList())
                 {
-                    foreach (DataTable dt in ds.Tables)
+                    for (DataTable dt : ds.Tables)
                     {
-                        if (dt.TableName != dtl.No)
+                        if (dt.TableName != dtl.getNo())
                             continue;
 
                         //删除原来的数据。
-                        GEDtl dtlEn = dtl.HisGEDtl;
-                        dtlEn.Delete(GEDtlAttr.RefPK, wk.OID.ToString());
+                        GEDtl dtlEn = dtl.getHisGEDtl();
+                        dtlEn.Delete(GEDtlAttr.RefPK, String.valueOf(wk.getOID()));
 
                         // 执行数据插入。
-                        foreach (DataRow drDtl in dt.Rows)
+                        for (DataRow drDtl : dt.Rows)
                         {
-                            if (drDtl["RefMainPK"].ToString() != mainPK)
+                            if (!drDtl.getValue("RefMainPK").toString().equals(mainPK))
                                 continue;
 
-                            dtlEn = dtl.HisGEDtl;
-                            foreach (DataColumn dc in dt.Columns)
-                                dtlEn.SetValByKey(dc.ColumnName, drDtl[dc.ColumnName].ToString());
+                            dtlEn = dtl.getHisGEDtl();
+                            for (DataColumn dc : dt.Columns)
+                                dtlEn.SetValByKey(dc.ColumnName, drDtl.getValue(dc.ColumnName).toString());
 
-                            dtlEn.RefPK = wk.OID.ToString();
-                            dtlEn.OID = 0;
+                            dtlEn.setRefPK(String.valueOf(wk.getOID()));
+                            dtlEn.setOID(0);
                             dtlEn.Insert();
                         }
                     }
@@ -304,40 +308,38 @@ public class AutoRunStratFlows extends Method{
             int toNodeID = 0;
             try
             {
-                toNodeID = int.Parse(dr["ToNode"].ToString());
-            }
-            catch
-            {
+                toNodeID =Integer.parseInt(dr.getValue("ToNode").toString());
+            }catch(Exception e){
                 /*有可能在4.5以前的版本中没有tonode这个约定.*/
             }
 
             // 处理发送信息.
             //  Node nd =new Node();
-            string msg = "";
+            String msg = "";
             try
             {
                 if (toNodeID == 0)
                 {
-                    WorkNode wn = new WorkNode(wk, fl.HisStartNode);
+                    WorkNode wn = new WorkNode(wk, fl.getHisStartNode());
                     msg = wn.NodeSend().ToMsgOfText();
                 }
 
-                if (toNodeID == fl.StartNodeID)
+                if (toNodeID == fl.getStartNodeID())
                 {
                     /* 发起后让它停留在开始节点上，就是为开始节点创建一个待办。*/
-                    Int64 workID = BP.WF.Dev2Interface.Node_CreateBlankWork(fl.No, null, null, WebUser.No, null);
-                    if (workID != wk.OID)
+                    long workID = BP.WF.Dev2Interface.Node_CreateBlankWork(fl.getNo(), null, null, WebUser.getNo(), null);
+                    if (workID != wk.getOID())
                         throw new Exception("@异常信息:不应该不一致的workid.");
                     else
                         wk.Update();
-                    msg = "已经为(" + WebUser.No + ") 创建了开始工作节点. ";
+                    msg = "已经为(" + WebUser.getNo() + ") 创建了开始工作节点. ";
                 }
 
                 BP.DA.Log.DefaultLogWriteLineInfo(msg);
             }
             catch (Exception ex)
             {
-                BP.DA.Log.DefaultLogWriteLineWarning("@" + fl.Name + ",第" + idx + "条,发起人员:" + WebUser.No + "-" + WebUser.Name + "发起时出现错误.\r\n" + ex.Message);
+                BP.DA.Log.DefaultLogWriteLineWarning("@" + fl.getName() + ",第" + idx + "条,发起人员:" + WebUser.getNo() + "-" + WebUser.getName() + "发起时出现错误.\r\n" + ex.getMessage());
             }
         }
         //#endregion 处理流程发起.

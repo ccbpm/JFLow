d8f69c083d4cfe0fccb83f765cbe2da9629df409
周朋 <hiflow@qq.com>
2018-08-28 09:31:06 +0800

    no commit message

---
+++ jflow-core/src/main/java/BP/WF/DTS/ccbpmServices.java
@@ -0,0 +1,70 @@
+package BP.WF.DTS;
+
+import BP.DA.DBAccess;
+import BP.DA.DataRow;
+import BP.DA.DataTable;
+import BP.DA.DataType;
+import BP.DA.LogType;
+import BP.En.Method;
+import BP.Port.Emp;
+import BP.WF.GenerWorkFlowAttr;
+import BP.WF.GenerWorkerList;
+import BP.WF.GenerWorkerListAttr;
+import BP.WF.GenerWorkerLists;
+import BP.WF.Node;
+import BP.WF.WorkNode;
+import BP.WF.Template.NodeAttr;
+import BP.WF.Template.OutTimeDeal;
+import cn.jflow.common.util.DateAndTime;
+
+public class ccbpmServices extends Method{
+
+	 /// <summary>
+    /// 不带有参数的方法
+    /// </summary>
+    public void ccbpmServices()
+    {
+        this.Title = "ccbpm服务";
+        this.Help = "扫描并处理逾期的任务，按照节点配置的预期规则,自动发起流程.";
+    }
+    /// <summary>
+    /// 设置执行变量
+    /// </summary>
+    /// <returns></returns>
+    public  void Init()
+    {
+    	
+    }
+    /// <summary>
+    /// 当前的操纵员是否可以执行这个方法
+    /// </summary>
+    public  boolean getIsCanDo()
+    {
+            return true;
+    }
+    /// <summary>
+    /// 执行
+    /// </summary>
+    /// <returns>返回执行结果</returns>
+    public  Object Do() throws Exception
+    {
+    	String info="";
+    	
+    	//执行触发方式发起的流程.
+    	AutoRunWF_Task wf_task=new AutoRunWF_Task();
+    	wf_task.Do();
+    	
+    	//执行触发方式发起的流程.
+    	AutoRunStratFlows flws=new AutoRunStratFlows();
+    	flws.Do();
+    	
+    	//处理超期的任务.
+    	AutoRunOverTimeFlow overTimeFlow=new AutoRunOverTimeFlow();
+    	overTimeFlow.Do();
+    	
+         
+        return info;
+    }
+ 
+}
+

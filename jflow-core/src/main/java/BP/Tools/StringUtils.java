f70085a3b1ca5b8344543746b1b677cdb115639f
袁丽娜 <nayuanli@126.com>
2018-10-26 17:30:25 +0800

    no commit message

---
+++ jflow-core/src/main/java/BP/Tools/StringUtils.java
@@ -0,0 +1,309 @@
+package BP.Tools;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.CharSequenceUtils;
+
+public class StringUtils {
+	public static final String EMPTY = "";
+	public static final int INDEX_NOT_FOUND = -1;
+	private static final int PAD_LIMIT = 8192;
+
+	/**
+	 * 判断是否为空
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isEmpty(String str) {
+		return str == null || str.length() == 0;
+	}
+	
+	public static boolean isEmpty(Object str) {
+		return (str == null || "".equals(str));
+	}
+
+	public static boolean isNotEmpty(String str) {
+		return !isEmpty(str);
+	}
+
+	public static boolean isBlank(String str) {
+		int strLen;
+		if (str == null || (strLen = str.length()) == 0) {
+			return true;
+		}
+		for (int i = 0; i < strLen; i++) {
+			if ((Character.isWhitespace(str.charAt(i)) == false)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isNotBlank(String str) {
+		return !StringUtils.isBlank(str);
+	}
+
+	public static String trim(String str) {
+		return str == null ? null : str.trim();
+	}
+
+	public static boolean startsWith(String str, String prefix) {
+		return startsWith(str, prefix, false);
+	}
+
+	public static boolean startsWithIgnoreCase(String str, String prefix) {
+		return startsWith(str, prefix, true);
+	}
+
+	private static boolean startsWith(String str, String prefix, boolean ignoreCase) {
+		if (str == null || prefix == null) {
+			return (str == null && prefix == null);
+		}
+		if (prefix.length() > str.length()) {
+			return false;
+		}
+		return str.regionMatches(ignoreCase, 0, prefix, 0, prefix.length());
+	}
+
+	public static boolean startsWithAny(String string, String[] searchStrings) {
+		if (isEmpty(string) || ArrayUtils.isEmpty(searchStrings)) {
+			return false;
+		}
+		for (int i = 0; i < searchStrings.length; i++) {
+			String searchString = searchStrings[i];
+			if (StringUtils.startsWith(string, searchString)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean endsWith(String str, String suffix) {
+		return endsWith(str, suffix, false);
+	}
+
+	public static boolean endsWithIgnoreCase(String str, String suffix) {
+		return endsWith(str, suffix, true);
+	}
+
+	private static boolean endsWith(String str, String suffix, boolean ignoreCase) {
+		if (str == null || suffix == null) {
+			return (str == null && suffix == null);
+		}
+		if (suffix.length() > str.length()) {
+			return false;
+		}
+		int strOffset = str.length() - suffix.length();
+		return str.regionMatches(ignoreCase, strOffset, suffix, 0, suffix.length());
+	}
+	 
+	
+	public static String rightPad(String str, int size) {
+		return rightPad(str, size, ' ');
+	}
+
+	public static String rightPad(String str, int size, char padChar) {
+		if (str == null) {
+			return null;
+		}
+		int pads = size - str.length();
+		if (pads <= 0) {
+			return str; // returns original String when possible
+		}
+		if (pads > PAD_LIMIT) {
+			return rightPad(str, size, String.valueOf(padChar));
+		}
+		return str.concat(padding(pads, padChar));
+	}
+
+	public static String rightPad(String str, int size, String padStr) {
+		if (str == null) {
+			return null;
+		}
+		if (isEmpty(padStr)) {
+			padStr = " ";
+		}
+		int padLen = padStr.length();
+		int strLen = str.length();
+		int pads = size - strLen;
+		if (pads <= 0) {
+			return str; // returns original String when possible
+		}
+		if (padLen == 1 && pads <= PAD_LIMIT) {
+			return rightPad(str, size, padStr.charAt(0));
+		}
+
+		if (pads == padLen) {
+			return str.concat(padStr);
+		} else if (pads < padLen) {
+			return str.concat(padStr.substring(0, pads));
+		} else {
+			char[] padding = new char[pads];
+			char[] padChars = padStr.toCharArray();
+			for (int i = 0; i < pads; i++) {
+				padding[i] = padChars[i % padLen];
+			}
+			return str.concat(new String(padding));
+		}
+	}
+
+	public static String leftPad(String str, int size) {
+		return leftPad(str, size, ' ');
+	}
+
+	public static String leftPad(String str, int size, char padChar) {
+		if (str == null) {
+			return null;
+		}
+		int pads = size - str.length();
+		if (pads <= 0) {
+			return str; // returns original String when possible
+		}
+		if (pads > PAD_LIMIT) {
+			return leftPad(str, size, String.valueOf(padChar));
+		}
+		return padding(pads, padChar).concat(str);
+	}
+
+	private static String padding(int repeat, char padChar) throws IndexOutOfBoundsException {
+		if (repeat < 0) {
+			throw new IndexOutOfBoundsException("Cannot pad a negative amount: " + repeat);
+		}
+		final char[] buf = new char[repeat];
+		for (int i = 0; i < buf.length; i++) {
+			buf[i] = padChar;
+		}
+		return new String(buf);
+	}
+
+	public static String leftPad(String str, int size, String padStr) {
+		if (str == null) {
+			return null;
+		}
+		if (isEmpty(padStr)) {
+			padStr = " ";
+		}
+		int padLen = padStr.length();
+		int strLen = str.length();
+		int pads = size - strLen;
+		if (pads <= 0) {
+			return str; // returns original String when possible
+		}
+		if (padLen == 1 && pads <= PAD_LIMIT) {
+			return leftPad(str, size, padStr.charAt(0));
+		}
+
+		if (pads == padLen) {
+			return padStr.concat(str);
+		} else if (pads < padLen) {
+			return padStr.substring(0, pads).concat(str);
+		} else {
+			char[] padding = new char[pads];
+			char[] padChars = padStr.toCharArray();
+			for (int i = 0; i < pads; i++) {
+				padding[i] = padChars[i % padLen];
+			}
+			return new String(padding).concat(str);
+		}
+	}
+
+	public static String replace(String text, String searchString, String replacement) {
+		return replace(text, searchString, replacement, -1);
+	}
+
+	public static String replace(String text, String searchString, String replacement, int max) {
+		if (isEmpty(text) || isEmpty(searchString) || replacement == null || max == 0) {
+			return text;
+		}
+		int start = 0;
+		int end = text.indexOf(searchString, start);
+		if (end == -1) {
+			return text;
+		}
+		int replLength = searchString.length();
+		int increase = replacement.length() - replLength;
+		increase = (increase < 0 ? 0 : increase);
+		increase *= (max < 0 ? 16 : (max > 64 ? 64 : max));
+		StringBuffer buf = new StringBuffer(text.length() + increase);
+		while (end != -1) {
+			buf.append(text.substring(start, end)).append(replacement);
+			start = end + replLength;
+			if (--max == 0) {
+				break;
+			}
+			end = text.indexOf(searchString, start);
+		}
+		buf.append(text.substring(start));
+		return buf.toString();
+	}
+	public static boolean hasLength(String str) {
+		return (str != null && str.length() > 0);
+	}
+	
+	public static String[] split(String toSplit, String delimiter) {
+		if (!hasLength(toSplit) || !hasLength(delimiter)) {
+			return null;
+		}
+		int offset = toSplit.indexOf(delimiter);
+		if (offset < 0) {
+			return null;
+		}
+		String beforeDelimiter = toSplit.substring(0, offset);
+		String afterDelimiter = toSplit.substring(offset + delimiter.length());
+		return new String[] {beforeDelimiter, afterDelimiter};
+	}
+	
+	public static String join(final Object[] array, final String separator) {
+        if (array == null) {
+            return null;
+        }
+        return join(array, separator, 0, array.length);
+    }
+	
+	 public static String join(final Object[] array, String separator, final int startIndex, final int endIndex) {
+	        if (array == null) {
+	            return null;
+	        }
+	        if (separator == null) {
+	            separator = EMPTY;
+	        }
+
+	        // endIndex - startIndex > 0:   Len = NofStrings *(len(firstString) + len(separator))
+	        //           (Assuming that all Strings are roughly equally long)
+	        final int noOfItems = endIndex - startIndex;
+	        if (noOfItems <= 0) {
+	            return EMPTY;
+	        }
+
+	        final StringBuilder buf = new StringBuilder(noOfItems * 16);
+
+	        for (int i = startIndex; i < endIndex; i++) {
+	            if (i > startIndex) {
+	                buf.append(separator);
+	            }
+	            if (array[i] != null) {
+	                buf.append(array[i]);
+	            }
+	        }
+	        return buf.toString();
+	    }
+	  public static String replacePattern(final String source, final String regex, final String replacement) {
+	        if (source == null || regex == null || replacement == null) {
+	            return source;
+	        }
+	        return Pattern.compile(regex, Pattern.DOTALL).matcher(source).replaceAll(replacement);
+	    }
+	  
+	  public static String[] toStringArray(Enumeration<String> enumeration) {
+			if (enumeration == null) {
+				return null;
+			}
+			List<String> list = Collections.list(enumeration);
+			return list.toArray(new String[list.size()]);
+		}
+}

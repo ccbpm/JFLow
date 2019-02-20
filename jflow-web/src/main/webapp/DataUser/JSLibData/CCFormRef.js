85eb09d23fe30c9253d6a58aecd997a2ec1733f5
袁丽娜 <nayuanli@126.com>
2019-02-20 15:56:01 +0800

    no commit message

--- jflow-web/src/main/webapp/DataUser/JSLibData/CCFormRef.js
+++ jflow-web/src/main/webapp/DataUser/JSLibData/CCFormRef.js
@@ -6,3 +6,23 @@
 function funDemo() {
     alert("我被执行了。");
 }
+
+function setShuZhi3Value(){
+	alert("我被执行了11。");
+}
+/**从表附件导入的固定方法，不可人为删除**/
+//FK_MapData,附件属性，RefPK,FK_Node
+function afterDtlImp(FK_MapData, frmAth, newOID, FK_Node, oldOID,oldFK_MapData) {
+    //处理从表附件导入的事件
+}
+
+function ChangeFormValue(form1,form2){
+	//根据WorkID 获取form1表单的数据
+	var handler = new HttpHandler("B")
+	var workID = GetQueryString("WorkID");
+	var en = new Entity("BP.Sys.GEEntity");
+	
+	en.OID = workID;
+	
+	
+}

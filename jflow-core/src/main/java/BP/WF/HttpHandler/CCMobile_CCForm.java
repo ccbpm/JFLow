b8b2e969d0b91994c9aea8187e5125a578bf917e
黄果 <772988+ccflowhg@users.noreply.gitee.com>
2019-03-29 16:54:31 +0800

    jmobile从表保存方法

--- jflow-core/src/main/java/BP/WF/HttpHandler/CCMobile_CCForm.java
+++ jflow-core/src/main/java/BP/WF/HttpHandler/CCMobile_CCForm.java
@@ -45,6 +45,122 @@ public class CCMobile_CCForm extends WebContralBase
         WF_CCForm ccform = new WF_CCForm(this.context);
         return ccform.Dtl_Init();
     }
+	
+	
+	/// <summary>
+    /// 保存从表数据
+    /// </summary>
+    /// <returns></returns>
     
+    public String Dtl_SaveRow() throws Exception
+    {
+        //#region  查询出来从表数据.
+        GEDtls dtls = new GEDtls(this.getEnsName());
+        GEDtl dtl = (GEDtl) dtls.getGetNewEntity();
+        dtls.Retrieve("RefPK", this.GetRequestVal("RefPKVal"));
+        Map map = dtl.getEnMap();
+        for (Entity item : dtls)
+        {
+            String pkval = item.GetValStringByKey(dtl.getPK());
+            for (Attr attr : map.getAttrs())
+            {
+                if (attr.getIsRefAttr() == true)
+                    continue;
+
+                if (attr.getMyDataType() == DataType.AppDateTime || attr.getMyDataType() == DataType.AppDate)
+                {
+                    if (attr.getUIIsReadonly() == true)
+                        continue;
+
+                    String val = this.GetValFromFrmByKey("TB_" + attr.getKey() + "_" + pkval, null);
+                    item.SetValByKey(attr.getKey(), val);
+                    continue;
+                }
+
+
+                if (attr.getUIContralType().equals(UIContralType.TB)  && attr.getUIIsReadonly() == false)
+                {
+                    String val = this.GetValFromFrmByKey("TB_" + attr.getKey() + "_" + pkval, null);
+                    item.SetValByKey(attr.getKey(), val);
+                    continue;
+                }
+
+                if (attr.getUIContralType().equals(UIContralType.DDL) && attr.getUIIsReadonly() == true)
+                {
+                    String val = this.GetValFromFrmByKey("DDL_" + attr.getKey() + "_" + pkval);
+                    item.SetValByKey(attr.getKey(), val);
+                    continue;
+                }
+
+                if (attr.getUIContralType().equals(UIContralType.CheckBok) && attr.getUIIsReadonly() == true)
+                {
+                    String val = this.GetValFromFrmByKey("CB_" + attr.getKey() + "_" + pkval, "-1");
+                    if (val == "-1")
+                        item.SetValByKey(attr.getKey(), 0);
+                    else
+                        item.SetValByKey(attr.getKey(), 1);
+                    continue;
+                }
+            }
+
+            item.Update(); //执行更新.
+        }
+        //#endregion  查询出来从表数据.
+
+        //#region 保存新加行.
+      
+        String keyVal = "";
+        for (Attr attr : map.getAttrs())
+        {
+
+            if (attr.getMyDataType() == DataType.AppDateTime || attr.getMyDataType() == DataType.AppDate)
+            {
+                if (attr.getUIIsReadonly() == true)
+                    continue;
+
+                keyVal = this.GetValFromFrmByKey("TB_" + attr.getKey() + "_0", null);
+                dtl.SetValByKey(attr.getKey(), keyVal);
+                continue;
+            }
+
+
+            if (attr.getUIContralType().equals(UIContralType.TB) && attr.getUIIsReadonly() == false)
+            {
+                keyVal = this.GetValFromFrmByKey("TB_" + attr.getKey() + "_0");
+                if (attr.getIsNum() && keyVal == "")
+                    keyVal = "0";
+                dtl.SetValByKey(attr.getKey(), keyVal);
+                continue;
+            }
+
+            if (attr.getUIContralType().equals(UIContralType.DDL) && attr.getUIIsReadonly() == true)
+            {
+                keyVal = this.GetValFromFrmByKey("DDL_" + attr.getKey() + "_0");
+                dtl.SetValByKey(attr.getKey(), keyVal);
+                continue;
+            }
+
+            if (attr.getUIContralType().equals(UIContralType.CheckBok) && attr.getUIIsReadonly() == true)
+            {
+                keyVal = this.GetValFromFrmByKey("CB_" + attr.getKey() + "_0", "-1");
+                if (keyVal == "-1")
+                    dtl.SetValByKey(attr.getKey(), 0);
+                else
+                    dtl.SetValByKey(attr.getKey(), 1);
+                continue;
+            }
+        }
+
+        dtl.SetValByKey("RefPK", this.GetRequestVal("RefPKVal"));
+        dtl.setPKVal("0");
+       
+		dtl.Insert();
+		
+        
+        //#endregion 保存新加行.
+
+        return "保存成功.";
+    }
+
 
 }

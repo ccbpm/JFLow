ccb364caff62f327ba77cf5419b6ee8dbdb697d7
周朋 <106651+ccflow@users.noreply.gitee.com>
2019-03-31 22:34:49 +0800

    no commit message

--- jflow-web/src/main/webapp/WF/Comm/Gener.js
+++ jflow-web/src/main/webapp/WF/Comm/Gener.js
@@ -1,21 +1,4 @@
-﻿////定义语言.
-//$(".lang").each(function (index, item) {
-//    var key = $(item).attr("data-key");
-//    var val = "";
-//    $.each(window.lang, function (i, n) {
-//        if (i == key) {
-//            val = n;
-//            return false;
-//        }
-
-//    });
-
-//    if (val.length > 0) {
-//        $(item).text(val);
-//    }
-
-//});
-
+﻿
 //检查字段,从表名,附件ID,输入是否合法.
 function CheckID(val) {
     //首位可以是字母以及下划线。 

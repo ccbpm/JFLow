ff50050ef3b4b4b707a47191628231b1d7ced785
袁丽娜 <1629967+linahaha@users.noreply.gitee.com>
2019-04-03 11:53:35 +0800

    no commit message

--- jflow-web/src/main/java/WebServiceImp/Test.java
+++ jflow-web/src/main/java/WebServiceImp/Test.java
@@ -1,13 +1,11 @@
 package WebServiceImp;
-import BP.Tools.HttpClientUtil;
 
-import java.util.Date;  
-import java.text.DateFormat;  
+import java.util.Hashtable;
+ 
 import org.apache.axis.client.Call;  
 import org.apache.axis.client.Service;  
 import javax.xml.namespace.QName;  
-import java.lang.Integer;  
-import javax.xml.rpc.ParameterMode;  
+
 
 public class Test {
 	
@@ -22,16 +20,28 @@ public class Test {
 	        Service service = new Service();  
 	        Call call = (Call) service.createCall();  
 	        call.setTargetEndpointAddress(webPath);  
-	        call.setOperationName(new QName("http://WebServiceImp","DB_Todolist"));// WSDL里面描述的接口名称  
-	        call.addParameter("userNo",  
+	        call.setOperationName(new QName("http://WebServiceImp","SendWork"));// WSDL里面描述的接口名称  
+	        call.addParameter("flowNo",  
 	                org.apache.axis.encoding.XMLType.XSD_DATE,  
 	                javax.xml.rpc.ParameterMode.IN);// 接口的参数  
-	        call.addParameter("sysNo",  
+	        call.addParameter("workid",  
+	                org.apache.axis.encoding.XMLType.XSD_DATE,  
+	                javax.xml.rpc.ParameterMode.IN);// 接口的参数  
+	        call.addParameter("ht",  
+	                org.apache.axis.encoding.XMLType.XSD_DATE,  
+	                javax.xml.rpc.ParameterMode.IN);// 接口的参数  
+	        call.addParameter("toNodeID",  
+	                org.apache.axis.encoding.XMLType.XSD_DATE,  
+	                javax.xml.rpc.ParameterMode.IN);// 接口的参数  
+	        call.addParameter("toEmps",  
 	                org.apache.axis.encoding.XMLType.XSD_DATE,  
 	                javax.xml.rpc.ParameterMode.IN);// 接口的参数  
 	        call.setReturnType(org.apache.axis.encoding.XMLType.XSD_STRING);// 设置返回类型  
 	        String temp = "admin";  
-	        String result = (String) call.invoke(new Object[] { temp,"" });  
+	        Hashtable ht = new Hashtable();
+	        ht.put("ZiDuan1", "111");
+	        ht.put("ZiDuan2", "222");
+	        String result = (String) call.invoke(new Object[] { "226",(long)798,ht,22602,"zhangyifan" });  
 	        // 给方法传递参数，并且调用方法  
 	        System.out.println("result is " + result);  
 	    } catch (Exception e) {  

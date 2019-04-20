ef741e7c823add0df3ad2743f8db1df26aa5f4d9
袁丽娜 <1629967+linahaha@users.noreply.gitee.com>
2019-04-20 15:00:53 +0800

    菜单树形结构

--- jflow-core/src/main/java/BP/WF/HttpHandler/WF_Comm.java
+++ jflow-core/src/main/java/BP/WF/HttpHandler/WF_Comm.java
@@ -407,7 +407,7 @@ public class WF_Comm extends WebContralBase {
             return "err@该实体["+this.getEnsName()+"]不是一个树形实体.";
         
         ens.RetrieveAll(EntityTreeAttr.Idx);
-        return ens.ToJsonOfTree("0");  
+        return BP.Tools.Json.ToJson(ens.ToDataTableField("TreeTable"));  
     }
     //#endregion 树的实体
     

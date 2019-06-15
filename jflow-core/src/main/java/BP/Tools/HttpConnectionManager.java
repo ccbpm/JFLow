1a8045a0a0c0bec8dead193f1a3f47ee27633271
袁丽娜 <nayuanli@126.com>
2019-06-15 17:33:08 +0800

    no commit message

---
+++ jflow-core/src/main/java/BP/Tools/HttpConnectionManager.java
@@ -0,0 +1,40 @@
+package BP.Tools;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
+public class HttpConnectionManager {
+
+	static PoolingHttpClientConnectionManager cm = null;
+	static {
+		LayeredConnectionSocketFactory sslsf = null;
+		try {
+			sslsf = new SSLConnectionSocketFactory(SSLContext.getDefault());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create()
+				.register("https", sslsf).register("http", new PlainConnectionSocketFactory()).build();
+		cm = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+		cm.setMaxTotal(200);
+		cm.setDefaultMaxPerRoute(20);
+	}
+
+	public static CloseableHttpClient getHttpClient() {
+		CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
+
+
+		return httpClient;
+	}
+}

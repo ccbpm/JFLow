3bfe670454450ab35d0baa3da4eefa76b92fda3d
黄果 <huangzhimin@ccflow.org>
2020-03-18 15:35:07 +0800

    no commit message

--- jflow-core/src/main/java/BP/WF/Flow.java
+++ jflow-core/src/main/java/BP/WF/Flow.java
@@ -6052,7 +6052,7 @@ public class Flow extends BP.En.EntityNoName {
 			md.Save();
 
 			// 装载模版.
-			String file = SystemConfig.getPathOfDataUser() + "XML/Templete/SheetOfStartNode.xml";
+			String file = SystemConfig.getPathOfDataUser() + "XML/TempleteSheetOfStartNode.xml";
 			if ((new File(file)).isFile() == false) {
 				throw new RuntimeException("@开始节点表单模版丢失" + file);
 			}

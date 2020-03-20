3e33ee6c4ac113e52c083988bec874fa8f36c713
黄果 <huangzhimin@ccflow.org>
2020-03-20 15:02:47 +0800

    no commit message

--- jflow-web/src/main/webapp/Portal/GPMMenus.js
+++ jflow-web/src/main/webapp/Portal/GPMMenus.js
@@ -6,39 +6,16 @@
 
 function InitLeftMenuComm() {
 
-    webUser = new WebUser();
+    webUser = new WebUser(appNo);
     if (webUser.No == null) {
         window.location.href = 'Login.htm';
         return;
     }
 
-    var sql1 = "SELECT No,Name,FK_Menu,ParentNo,Url,Tag1,Tag2,Tag3,WebPath,Icon,Idx ";
-    sql1 += " FROM v_gpm_empmenu ";
-    sql1 += " WHERE FK_Emp = '" + webUser.No + "' ";
-    sql1 += " AND MenuType = '3' ";
-    sql1 += " AND FK_App = '" + appNo + "' ";
-    sql1 += " UNION ";  //加入不需要权限控制的菜单.
-    sql1 += "SELECT No,Name, No as FK_Menu,ParentNo,Url,Tag1,Tag2,Tag3,WebPath,Icon,Idx";
-    sql1 += " FROM GPM_Menu ";
-    sql1 += " WHERE MenuCtrlWay=1 ";
-    sql1 += " AND MenuType = '3' ";
-    sql1 += " AND FK_App = '" + appNo + "' ORDER BY Idx ";
-
-    var dirs = DBAccess.RunSQLReturnTable(sql1);
-
-    var sql2 = "SELECT No,Name,FK_Menu,ParentNo,Url,Tag1,Tag2,Tag3,WebPath,Icon,Idx ";
-    sql2 += " FROM v_gpm_empmenu ";
-    sql2 += " WHERE FK_Emp = '" + webUser.No + "'";
-    sql2 += " AND MenuType = '4' ";
-    sql2 += " AND FK_App = '" + appNo + "' ";
-    sql2 += " UNION ";  //加入不需要权限控制的菜单.
-    sql2 += "SELECT No,Name, No as FK_Menu,ParentNo,Url,Tag1,Tag2,Tag3,WebPath,Icon,Idx ";
-    sql2 += " FROM GPM_Menu "; //加入不需要权限控制的菜单.
-    sql2 += " WHERE MenuCtrlWay=1 ";
-    sql2 += " AND MenuType = '4' ";
-    sql2 += " AND FK_App = '" + appNo + "' ORDER BY Idx ";
-
-    var menus = DBAccess.RunSQLReturnTable(sql2);
+    //通过API获得两个数据源.
+    var data = GPM_GenerMenumsDB();
+    var dirs = data["Dirs"]; //获得目录.
+    var menus = data["Menus"]; //获得菜单.
 
     var firstIcon = "icon-folder";
     var secondIcon = "icon-book-open";
@@ -52,9 +29,9 @@ function InitLeftMenuComm() {
         if (icon != null && icon != "")
             firstIcon = icon;
 
-        var url = dir.Url;
+        var url = dir.UrlExt;
         if (url == undefined)
-            url = dir.URL;
+            url = dir.URLEXT;
 
         var name = dir.Name;
         if (name == undefined)
@@ -89,9 +66,9 @@ function InitLeftMenuComm() {
                 if (icon != null && icon != "")
                     secondIcon = icon;
 
-                var url = menu.Url;
+                var url = menu.UrlExt;
                 if (url == undefined)
-                    url = menu.URL;
+                    url = menu.URLEXT;
 
                 var name = menu.Name;
                 if (name == undefined)
@@ -106,7 +83,8 @@ function InitLeftMenuComm() {
     });
 }
 
-
 function GotoUrl(url) {
     $('#J_iframe').attr('src', url);
 }
+
+

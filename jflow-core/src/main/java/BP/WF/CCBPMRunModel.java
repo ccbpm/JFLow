e15c0a18f7a3b476d7e41faf3828a6679fd65f24
黄果 <huangzhimin@ccflow.org>
2020-03-31 09:58:22 +0800

    no commit message

---
+++ jflow-core/src/main/java/BP/WF/CCBPMRunModel.java
@@ -0,0 +1,28 @@
+package BP.WF;
+
+public enum CCBPMRunModel {
+    /**
+     单机版
+     */
+    Single,
+    /**
+     集团模式
+     */
+    GroupInc,
+    /**
+     多租户模式
+     */
+    SAAS;
+
+    public static final int SIZE = java.lang.Integer.SIZE;
+
+    public int getValue()
+    {
+        return this.ordinal();
+    }
+
+    public static CCBPMRunModel forValue(int value)
+    {
+        return values()[value];
+    }
+}

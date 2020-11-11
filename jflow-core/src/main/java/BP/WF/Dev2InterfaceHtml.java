ba7b423662ce0433b941dd4e708444b4455d89f0
袁丽娜 <nayuanli@126.com>
2020-11-11 09:12:58 +0800

    no commit message

--- jflow-core/src/main/java/BP/WF/Dev2InterfaceHtml.java
+++
@@ -1,9 +0,0 @@
-package BP.WF;
-
-public class Dev2InterfaceHtml
-{
-	public final String getBtn_Return()
-	{
-		return " onclick='' ";
-	}
-}
--- jflow-core/src/main/java/BP/WF/Template/FrmEleType.java
+++
@@ -1,9 +0,0 @@
-package BP.WF.Template;
-
-import BP.WF.*;
-
-public class FrmEleType
-{
-	public static final String Field = "Field";
-	public static final String Dtl = "Dtl";
-}
